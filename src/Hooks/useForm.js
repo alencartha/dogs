ba8bb4bf3e5ca10cfd04a600b6b359ccbd3176c5
alencartha <alencar_tha@hotmail.com>
2023-01-18 20:09:29 -0300

    formulario, hook validacao, rotas

---
+++ src/Hooks/useForm.js
@@ -0,0 +1,49 @@
+import React from 'react'
+
+const types = {
+    email: {
+        regex: /^[\w+.]+@\w+\.\w{2,}(?:\.\w{2})?$/,
+        message: "Preencha um e-mail válido"
+    }
+}
+
+const useForm = (type) => {
+
+    const [value, setValue] = React.useState('')
+    const [error, setError] = React.useState(null)
+
+    function onChange({ target }) {
+
+        if(error){
+            validate(target.value)
+        }
+        setValue(target.value)
+    }
+
+    function validate(value) {
+
+        if (type === false) return true
+
+        if (value.length === 0) {
+            setError("Preencha um valor.")
+            return false
+        } else if (types[type] && !types[type].regex.test(value)) {
+            setError(types[type].message)
+            return false
+        } else {
+            setError(null)
+            return true
+        }
+    }
+
+    return {
+        value,
+        setValue,
+        onChange,
+        validate: () => validate(value),
+        onBlur: () => validate(value),
+        error
+    }
+}
+
+export default useForm
