c9ff6d1dd217ac353b80aa61c3856cc6e3ff3bed
alencartha <alencar_tha@hotmail.com>
2023-01-25 13:14:53 -0300

    style: ajuste grafico

--- src/Components/User/UserStatsGraphs.js
+++ src/Components/User/UserStatsGraphs.js
@@ -1,44 +1,55 @@
-import React from 'react'
-import styles from './UserStatsGraphs.module.css'
-import { VictoryPie, VictoryChart, VictoryBar } from 'victory'
+import React from 'react';
+import styles from './UserStatsGraphs.module.css';
+import { VictoryPie, VictoryChart, VictoryBar } from 'victory';
 
 const UserStatsGraphs = ({ data }) => {
+  const [graph, setGraph] = React.useState([]);
+  const [total, setTotal] = React.useState(0);
 
-    const [graph, setGraph] = React.useState([])
-    const [total, setTotal] = React.useState(0)
+  React.useEffect(() => {
+    const graphData = data.map((item) => {
+      return {
+        x: item.title,
+        y: Number(item.acessos),
+      };
+    });
 
-    React.useEffect(() => {
+    setTotal(
+      data.map(({ acessos }) => Number(acessos)).reduce((a, b) => a + b, 0),
+    );
+    setGraph(graphData);
+  }, [data]);
 
-        const graphData = data.map(item => {
-            return {
-                x: item.title,
-                y: Number(item.acessos)
-            }
-        })
+  return (
+    <section className={`${styles.graph} animeLeft`}>
+      <div className={`${styles.total} ${styles.graphItem}`}>
+        <p>Acessos: {total}</p>
+      </div>
+      <div className={styles.graphItem}>
+        <VictoryPie
+          data={graph}
+          innerRadius={50}
+          padding={{ top: 20, bottom: 20, left: 80, right: 80 }}
+          style={{
+            data: {
+              fillOpacity: 0.9,
+              stroke: '#fff',
+              strokeWidth: 2,
+            },
+            labels: {
+              fontSize: 14,
+              fill: '#333',
+            },
+          }}
+        />
+      </div>
+      <div className={styles.graphItem}>
+        <VictoryChart>
+          <VictoryBar alignment="start" data={graph}></VictoryBar>
+        </VictoryChart>
+      </div>
+    </section>
+  );
+};
 
-        setGraph(graphData)
-        setTotal(data.map(({ acessos }) => Number(acessos)).reduce((a, b) => a + b))
-
-    }, [data])
-
-    return (
-        <section className={`animeLeft, ${styles.graph}`}>
-            <div className={`${styles.total} ${styles.graphItem} `}>
-                <p>Acessos: {total}</p>
-            </div>
-
-            <div className={styles.graphItem} >
-                <VictoryPie data={graph} innerRadius={50} padding={{ top: 20, bottom: 40, left: 20, right: 20 }} style={{ data: { fillOpacity: .9, stroke: '#fff', stokeWidth: 2 }, labels: { fontSize: 14, fill: '#333' } }} />
-            </div>
-
-            <div className={styles.graphItem}>
-                <VictoryChart>
-                    <VictoryBar alignment='start' data={graph}></VictoryBar>
-                </VictoryChart>
-            </div>
-
-        </section>
-    )
-}
-
-export default UserStatsGraphs
+export default UserStatsGraphs;
